51311d2f169c7b55ded72268cf4ac0c17b98c408
lyoodong <dwryu3079@naver.com>
2023-09-07 23:43:46 +0900

    [ADD]: ReusableCollectionView, Cell 파일 생성

--- Source/Presenter/Global/ReusableCollectionView/ReusableCollectionViewCell.swift
+++ Source/Presenter/Global/ReusableCollectionView/ReusableCollectionViewCell.swift
@@ -1,14 +1,8 @@
 //
-//  SearchView.swift
+//  ReusableCollectionViewCell.swift
 //  SecondRecap
 //
 //  Created by Dongwan Ryoo on 2023/09/07.
 //
 
-import UIKit
-
-class SearchView: BaseView {
-
-
-
-}
+import Foundation
