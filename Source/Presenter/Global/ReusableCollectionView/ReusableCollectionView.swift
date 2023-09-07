0ab2a8a2e78cb8d486d904c19a0472de4396154f
lyoodong <dwryu3079@naver.com>
2023-09-07 23:52:32 +0900

    [ADD]:  ReusableCollectionView, Cell 초기 코드 생성

--- Source/Presenter/Global/ReusableCollectionView/ReusableCollectionView.swift
+++ Source/Presenter/Global/ReusableCollectionView/ReusableCollectionView.swift
@@ -5,4 +5,16 @@
 //  Created by Dongwan Ryoo on 2023/09/07.
 //
 
-import Foundation
+import UIKit
+
+class ReusableCollectionView: UICollectionView {
+
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        register(ReusableCollectionViewCell.self, forCellWithReuseIdentifier: ReusableCollectionViewCell.IDF)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Source/Presenter/Global/ReusableCollectionView/ReusableCollectionViewCell.swift
+++ Source/Presenter/Global/ReusableCollectionView/ReusableCollectionViewCell.swift
@@ -7,7 +7,15 @@
 
 import UIKit
 
-//ReusableIDF
-protocol ReusableIDF {
-    static var IDF:String { get }
+class ReusableCollectionViewCell: BaseCollectionViewCell {
+
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
