21261b019e0cbaaa6dd93a286f8607a1da29a655
lyoodong <dwryu3079@naver.com>
2023-09-07 23:51:38 +0900

    [ADD]:  ReusableIDF 프로토콜 파일 생성

---
+++ Source/Presenter/Global/Protocol/ Pro + ReusableIDF.swift
@@ -0,0 +1,13 @@
+//
+//  Pro + ReusableIDF.swift
+//  SecondRecap
+//
+//  Created by Dongwan Ryoo on 2023/09/07.
+//
+
+import Foundation
+
+//ReusableIDF
+protocol ReusableIDF {
+    static var IDF:String { get }
+}
