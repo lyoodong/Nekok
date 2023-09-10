233837d20761267e9fc9cb4e28a3da430828d873
lyoodong <dwryu3079@naver.com>
2023-09-10 23:26:53 +0900

    [ADD]: 모델 코드 생성

--- Data/Model/RealmModel.swift
+++ Data/Model/RealmModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import RealmSwift
+
+class liked: Object {
+    @Persisted var title: String
+    @Persisted var link: String
+    @Persisted var image: String
+    @Persisted var lprice: String
+    @Persisted var mallName: String
+    @Persisted var productID: String
+    
+    convenience init(title: String, link: String, image: String, lprice: String, mallName: String, productID: String   ) {
+        self.init()
+        self.title = title
+        self.link = link
+        self.image = image
+        self.lprice = lprice
+        self.mallName = mallName
+        self.productID = productID
+    }
+}
