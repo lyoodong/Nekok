278c3464eaf7dc6f46ab791a03658eb027312c75
lyoodong <dwryu3079@naver.com>
2023-09-11 16:44:07 +0900

    [ADD]: Ext + UIFont 추가

---
+++ Source/Presenter/Global/Extension/Ext + UIFont.swift
@@ -0,0 +1,14 @@
+//
+//  Ext + UIFont.swift
+//  SecondRecap
+//
+//  Created by Dongwan Ryoo on 2023/09/11.
+//
+
+import UIKit
+
+extension UIFont {
+    static let basicFont = UIFont.systemFont(ofSize: 12, weight: .regular)
+    static let productTitle = UIFont.systemFont(ofSize: 14, weight: .regular)
+    static let productPrice = UIFont.systemFont(ofSize: 16, weight: .bold)
+}
