3959b6dd29578afea147c4637de8927dade6d7c4
lyoodong <dwryu3079@naver.com>
2023-09-11 17:09:15 +0900

    [ADD]: ProductID 기본키 지정(한참 이전 작업인데 커밋 누락으로 지금 반영)

--- Data/Model/RealmModel.swift
+++ Data/Model/RealmModel.swift
@@ -8,13 +8,13 @@
 import Foundation
 import RealmSwift
 
-class liked: Object {
+class RealmModel: Object {
     @Persisted var title: String
     @Persisted var link: String
     @Persisted var image: String
     @Persisted var lprice: String
     @Persisted var mallName: String
-    @Persisted var productID: String
+    @Persisted (primaryKey: true)var productID: String
     
     convenience init(title: String, link: String, image: String, lprice: String, mallName: String, productID: String   ) {
         self.init()
