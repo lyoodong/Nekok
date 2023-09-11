4a4ee30339ab773792bef5dd562887802609e68f
lyoodong <dwryu3079@naver.com>
2023-09-11 16:44:24 +0900

    [ADD]: Ext + String 추가

---
+++ Source/Presenter/Global/Extension/Ext + String.swift
@@ -0,0 +1,18 @@
+//
+//  Ext + String.swift
+//  SecondRecap
+//
+//  Created by Dongwan Ryoo on 2023/09/11.
+//
+
+import Foundation
+
+extension String {
+
+    var emptyStrng: String {
+        get {
+            return ""
+        }
+    }
+    
+}
